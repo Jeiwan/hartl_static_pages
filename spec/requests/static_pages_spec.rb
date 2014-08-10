9c078ddb40727bf08891c1eb4d8839815be55a67
Ivan Kuznetsov <me@jeiwan.ru>
2014-08-10 23:41:14 +0400

    some changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,28 +11,29 @@ RSpec.describe "Static pages", :type => :request do
   subject { page }
   describe "Help page" do
     before { visit help_path }
-    it { should have_content('Help page') }
+    it { should have_selector('h1', text: 'Help page') }
     it { should have_title('Help') }
   end
 
   subject { page }
   describe "About page" do
     before { visit about_path }
-    it { should have_content('About us') }
+    it { should have_selector('h1', text: 'About us') }
     it { should have_title('About') }
   end
 
   subject { page }
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_content('Contact us') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title('Contact') }
   end
 
   subject { page }
   describe "Other page" do
     before { visit other_path }
-    it { should have_content('Other page') }
+    it { should have_selector('h1', text: 'Other page') }
     it { should have_title('Another page') }
   end
+
 end
